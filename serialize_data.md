8f716d6affd5bf4f36b1a3214b2eb9adf9fa7f10
Kac321 <01187312@pw.edu.pl>
2024-12-18 11:55:48 +0000

    fix(serialize_data): drobne poprawki
    Sprawienie, że funkcję serialize_data() można zaimportować i wywołać z innego skryptu Python (Poprzednio należało wywołać ją z konsoli i wynik wypisywała na konsoli)
    
    Poprawienie serialize_data.md, tak aby odzwierciedlało powyższe zmiany

--- serialize_data.md
+++ serialize_data.md
@@ -1,10 +1,15 @@
 # Jak używać skryptu serialize_data.py
-Aby użyć skryptu należy wywołać go za pomocą interpretera python z następującymi argumentami pozycyjnymi (kolejność ma znaczenie):
-1. Ścieżka do pliku z próbkami sygnału (float32)
-2. Okres próbkowania (w sekundach)
+Aby użyć skryptu należy wywołać funkcję main(path, sample_period) z argumentami:
+1. path - Ścieżka bezwzględna do przetwarzanego pliku
+2. sample_period - Okres próbkowania (w sekundach)
 
-# Przykładowe użycie
-Poniższe wywołanie skryptu wypisze na konsoli w postaci JSON próbki sygnału z pliku "WAR_2024-10-21_10:04:01" (należy zwrócić uwagę na odwołanie znaków specjalnych), zakładając okres próbkowania 5 sekund
+# Co zwraca skrypt?
+Skrypt zwraca text/JSON, w formacie:
 ```
-python3.10 serialize_data.py ../../test_data/WAR_2024-10-21_10\:04\:01 5
+[
+    {
+        "date": <datetime>,
+        "close": <wartość_próbki>
+    }
+]
 ```
--- web_server/serialize_data.py
+++ web_server/serialize_data.py
@@ -1,34 +1,24 @@
-from sys import argv
-from numpy import fromfile, float32, nditer
+from numpy import fromfile, float32
 from datetime import datetime, timedelta
 import json
 
-okres_probkowania = int(argv[2])
-antenna = argv[1].split('/')[-1].split('_')[0]
-date = argv[1].split('/')[-1].split('_')[1]
-raw_data = fromfile(open(argv[1]), dtype = float32)
-time = datetime.strptime(argv[1].split('/')[-1].split('_')[2], '%H:%M:%S') - datetime(1900,1,1) - timedelta(seconds=okres_probkowania*(len(raw_data)-1)) + datetime(int(date.split('-')[0]), int(date.split('-')[1]), int(date.split('-')[2]))
+def serialize_data(path, sample_period):
+    antenna = path.split('/')[-1].split('_')[0]
+    date = path.split('/')[-1].split('_')[1]
+    raw_data = fromfile(open(path), dtype = float32)
+    time = datetime.strptime(path.split('/')[-1].split('_')[2], '%H:%M:%S') - datetime(1900,1,1) - timedelta(seconds=sample_period*(len(raw_data)-1)) + datetime(int(date.split('-')[0]), int(date.split('-')[1]), int(date.split('-')[2]))
 
-indexes = [ i for i in range(0, len(raw_data)) ]
-to_json = [{"date": time, "close": str(raw_data[0])}]
-for key, sample in nditer([indexes, raw_data]):
-    if key == 0:
-        continue
-    to_json.append({"date": to_json[key-1]["date"] + timedelta(seconds=5), "close": str(sample)})
+    to_json = []
+    for key in range(0, len(raw_data)):
+        if key == 0:
+            to_json.append({"date": time, "close": str(raw_data[key])})
+        else:
+            to_json.append({"date": to_json[key-1]["date"] + timedelta(seconds=sample_period), "close": str(raw_data[key])})
 
-class DateTimeJSONEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, datetime):
-            return obj.isoformat()
-        return super(DateTimeJSONEncoder, self).default(obj)
+    class DateTimeJSONEncoder(json.JSONEncoder):
+        def default(self, obj):
+            if isinstance(obj, datetime):
+                return obj.isoformat()
+            return super(DateTimeJSONEncoder, self).default(obj)
 
-ready = json.dumps(to_json, cls=DateTimeJSONEncoder, indent=4)
-print(ready)
-
-
-
-
-
-
-
-
+    return json.dumps(to_json, cls=DateTimeJSONEncoder, indent=4)
