63cab2520c5c8c597bdd963b5595c01390f56092
kro1118 <karol.pacwa1118@gmail.com>
2025-01-05 00:20:55 +0100

    fix: dodanie nowych funkcji do serwera sieciowego dodanie funkcji eksportujących dodanie legendy i regulowanej daty oraz osi OY dodanie wyboru anten dodanie funkcji sprawdzających aktualność danych

--- web_server/server.py
+++ web_server/server.py
@@ -1,10 +1,69 @@
-from flask import Flask, render_template
+import json
+import requests
+import threading
+import time
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+external_servers = {
+    "data1": "http://external-server1.com/api/data1",
+    "data2": "http://external-server2.com/api/data2",
+    "data3": "http://external-server3.com/api/data3",
+}
+
+# Function to update the data with 1800 second interval
+def fetch_and_update_data(antenna_key, url):
+    try:
+        response = requests.get(url, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+
+        with open(f'./web_server/static/randomdata/{antenna_key}.json', 'w') as f:
+            json.dump(data, f)
+        print(f"Data {antenna_key} has been updated.")
+    except (requests.RequestException, json.JSONDecodeError) as e:
+        print(f"An error occured during the update of {antenna_key}: {e}")
+
+def periodic_sync(interval=1800):
+    while True:
+        print("Starting the update of data...")
+        for antenna, url in external_servers.items():
+            fetch_and_update_data(antenna, url)
+        print("Update finished. Next one in 30 minutes.")
+        time.sleep(interval)
+
+# Adding routes
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/data1')
+def get_data1():
+    return get_local_data('data1')
+
+@app.route('/data2')
+def get_data2():
+    return get_local_data('data2')
+
+@app.route('/data3')
+def get_data3():
+    return get_local_data('data3')
+
+def get_local_data(antenna_key):
+    try:
+        with open(f'./web_server/static/randomdata/{antenna_key}.json', 'r') as f:
+            data = json.load(f)
+        return jsonify(data)
+    except FileNotFoundError:
+        return jsonify({'error': 'File not found'}), 404
+    except json.JSONDecodeError:
+        return jsonify({'error': 'Invalid JSON data'}), 500
+
 if __name__ == '__main__':
+    # Synchronization in another thread
+    sync_thread = threading.Thread(target=periodic_sync, daemon=True)
+    sync_thread.start()
+
+    # Turn on server
     app.run(host='0.0.0.0', port=5000)
--- web_server/static/script.js
+++ web_server/static/script.js
@@ -6,8 +6,39 @@ const marginRight = 30;
 const marginBottom = 30;
 const marginLeft = 40;
 
+// selectors of antennas
+const checkbox1 = document.getElementById('A1');
+const checkbox2 = document.getElementById('A2');
+const checkbox3 = document.getElementById('A3');
+
+// colorScale for each antenna
+const colorScale = d3.scaleOrdinal()
+    .domain(["Antena1", "Antena2", "Antena3"]) 
+    .range(["#1f77b4", "#ff7f0e", "#2ca02c"]);
+
+
+let filteredData = []; 
+
+// Function to iterate the antennas selected by the user
+function iterateSelectedAntennas(selectedAntennas, callback) {
+    if (selectedAntennas && Array.isArray(selectedAntennas)) {
+      for (const antenna of selectedAntennas) {
+        callback(antenna);
+      }
+    }
+  }
+// Function to set the maximum date to current time
+function setMaxDate() {
+    const now = new Date();
+    const formattedDate = now.toISOString().slice(0, 16);
+    document.getElementById('start').max = formattedDate;
+    document.getElementById('end').max = formattedDate;
+}
+window.onload = setMaxDate;
+
 // Function to draw the chart with JSON data
-function drawChart(aapl) {
+function drawChart(aapl, startTime, endTime, minValue, maxValue) {
+
     // Map data if needed
     const aaplMissing = aapl.map(d => ({
         ...d,
@@ -15,21 +46,24 @@ function drawChart(aapl) {
     }));
 
     // Declare the x (horizontal position) scale.
-    const x = d3.scaleUtc()
-        .domain(d3.extent(aapl, d => d.date))
-        .range([marginLeft, width - marginRight]);
+    const x = d3.scaleTime() 
+    .domain([startTime, endTime])
+    .range([0, width]);
 
     // Declare the y (vertical position) scale.
     const y = d3.scaleLinear()
-        .domain([0, d3.max(aapl, d => d.close)])
+        .domain([maxValue,minValue])
         .nice()
         .range([marginTop,height - marginBottom ]);
 
     // Declare the line generator.
-    const line = d3.line()
-        .defined(d => !isNaN(d.close))
-        .x(d => x(d.date))
-        .y(d => y(d.close));
+    const line= d3.line()
+    .x(d => x((d.date)))  
+    .y(d => y(d.close));
+
+    // Group data by antenna (assuming aapl has an 'antenna' property)
+    const dataByAntenna = d3.group(aapl, d => d.antenna);
+
 
     // Clear existing SVG if any
     d3.select("#container").select("svg").remove();
@@ -41,62 +75,234 @@ function drawChart(aapl) {
         .attr("viewBox", [0, 0, width, height])
         .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
 
-    // Add the x-axis.
-    svg.append("g")
-        .attr("transform", `translate(0,${height - marginBottom})`)
-        .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
+   // Draw each antenna's data
+   dataByAntenna.forEach((data, antenna) => {
 
-    // Add the y-axis, remove the domain line, add grid lines and a label.
-    svg.append("g")
-        .attr("transform", `translate(${marginLeft},0)`)
-        .call(d3.axisLeft(y).ticks(height / 40))
-        .call(g => g.select(".domain").remove())
-        .call(g => g.selectAll(".tick line").clone()
-            .attr("x2", width - marginLeft - marginRight)
-            .attr("stroke-opacity", 0.1))
-        .call(g => g.append("text")
-            .attr("x", -marginLeft)
-            .attr("y", 10)
-            .attr("fill", "currentColor")
-            .attr("text-anchor", "start")
-            .text("Wartość w dB"));
+    // Filter out invalid or missing data for each antenna
+    const validData = data.filter(d => !isNaN(d.close) && d.date >= startTime && d.date <= endTime);
+    const missingData = data.filter(d => isNaN(d.close) && d.date >= startTime && d.date <= endTime);
 
-    // Append a path for the background line.
+    // Draw path for valid data
     svg.append("path")
+        .datum(validData)
         .attr("fill", "none")
-        .attr("stroke", "#ccc")
-        .attr("stroke-width", 1.5)
-        .attr("d", line(aaplMissing.filter(d => !isNaN(d.close))));
+        .attr("stroke", colorScale(antenna))
+        .attr("stroke-width", 2)
+        .attr("d", line);
 
-    // Append a path for the main line.
+    // Draw path for missing data (with dashed line)
     svg.append("path")
+        .datum(missingData)
         .attr("fill", "none")
-        .attr("stroke", "steelblue")
-        .attr("stroke-width", 1.5)
-        .attr("d", line(aaplMissing));
+        .attr("stroke", "gray")
+        .attr("stroke-dasharray", "4,4")  
+        .attr("stroke-width", 2)
+        .attr("d", line);
+});
+
+// Adding X and Y axes
+svg.append("g")
+    .attr("transform", `translate(0,${height - marginBottom})`)
+    .call(d3.axisBottom(x).ticks(width / 80).tickSizeOuter(0));
+
+svg.append("g")
+    .attr("transform", `translate(${marginLeft},0)`)
+    .call(d3.axisLeft(y).ticks(height / 40))
+    .call(g => g.select(".domain").remove())
+    .call(g => g.selectAll(".tick line").clone()
+        .attr("x2", width - marginLeft - marginRight)
+        .attr("stroke-opacity", 0.1))
+    .call(g => g.append("text")
+        .attr("x", -marginLeft)
+        .attr("y", 10)
+        .attr("fill", "currentColor")
+        .attr("text-anchor", "start")
+        .text("Wartość w dB"));
+addLegend(svg, Array.from(dataByAntenna.keys()), colorScale);
+
 }
-
 
 //Button - Fetch JSON Data and Update Chart
 document.getElementById("fetch-data").addEventListener("click", () => {
-    fetch("http://localhost/data")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            // Convert JSON data to the format expected by drawChart
-            const formattedData = data.map(d => ({
-                date: new Date(d.date),  // Assuming data contains 'date' in ISO format
-                close: +d.close           // Assuming data contains 'close' as a number
-            }));
 
-            // Draw the chart with the fetched JSON data
-            drawChart(formattedData);
+    // Checking the indputs from html file
+
+    const selectedAntennas = [];
+    if (checkbox1.checked) selectedAntennas.push("Antena1");
+    if (checkbox2.checked) selectedAntennas.push("Antena2");
+    if (checkbox3.checked) selectedAntennas.push("Antena3");
+
+    const startTime = new Date(document.getElementById('start').value);
+    const endTime = new Date(document.getElementById('end').value);
+
+    const minValue = Number(document.getElementById('min').value);
+    const maxValue = Number(document.getElementById('max').value);
+
+
+
+    // Checking whether the correct time period is chosen
+
+    if (isNaN(startTime) || isNaN(endTime)) {
+        alert('Podaj prawidłowy przedział czasowy.');
+        return;
+    }
+    if (startTime >= endTime) {
+        alert('Data początkowa musi być wcześniejsza niż data końcowa.');
+        return;
+    }
+    if (selectedAntennas.length === 0) {
+        alert('Wybierz co najmniej jedną antenę.');
+        return;
+    }
+
+    alert(`Wybrany przedział czasowy: ${startTime.toLocaleString()} - ${endTime.toLocaleString()}`);
+
+        const promises = selectedAntennas.map(antenna => {
+            const nr = antenna.slice(-1);
+            return fetch(`http://localhost:5000/data${nr}`)
+                .then(response => response.json())
+                .then(result => {
+
+                    // Checking the data format
+                    if (!Array.isArray(result) || result.length === 0) {
+                        console.error(`Brak danych dla ${antenna}. Odpowiedź serwera:`, result);
+                        alert(`Brak danych dla ${antenna}.`);
+                        return [];
+                    }
+    
+                    // Data processing
+                    const antennaData = result.map(d => {
+                        if (d.date && d.close) {
+                            return {
+                                date: new Date(d.date),
+                                close: parseFloat(d.close),
+                                antenna: antenna,
+                            };
+                        } else {
+                            console.warn(`Nieprawidłowe dane dla ${antenna}:`, d);
+                            return null;
+                        }
+                    }).filter(d => d !== null);
+    
+                    if (antennaData.length === 0) {
+                        console.warn(`Brak prawidłowych danych w tablicy dla ${antenna}.`);
+                        alert(`Brak prawidłowych danych dla ${antenna}.`);
+                        return [];
+                    }
+    
+                    // Checking the date of last date
+                    const latestDataDate = antennaData[antennaData.length - 1].date;
+                    const currentDate = new Date();
+                    const timeDifference = (currentDate - latestDataDate) / (1000 * 60); 
+    
+                    if (timeDifference > 30) {
+                        const simpleDate = latestDataDate.toLocaleString('en-GB', {
+                            day: '2-digit',
+                            month: '2-digit',
+                            year: 'numeric',
+                            hour: '2-digit',
+                            minute: '2-digit',
+                            hour12: false
+                        });
+                        alert(`Dane dla ${antenna} nie zostały zaktualizowane od ${simpleDate}.`);
+                    }
+                    return antennaData;
+                })
+                .catch(error => {
+                    console.error(`Error fetching data for ${antenna}:`, error);
+                    alert(`Wystąpił problem z pobieraniem danych dla ${antenna}.`);
+                    return [];
+                });
+        });
+
+
+        Promise.all(promises)
+        .then(results => {
+            const aapl = results.flat();
+
+            // Filtering the data by date
+            filteredData = aapl.filter(d => d.date >= startTime && d.date <= endTime);
+
+            if (filteredData.length === 0) {
+                alert('Brak danych do wyświetlenia dla wybranego przedziału czasowego.');
+                return;
+            }
+            drawChart(filteredData, startTime, endTime, minValue, maxValue);
         })
         .catch(error => {
             console.error("Error fetching data:", error);
         });
 });
+document.getElementById("export-data").addEventListener("click", () => {
+    // Checking whether there is something to export
+    if (!filteredData || filteredData.length === 0) {
+        alert('Brak danych do eksportu.');
+        return;
+    }
+
+    //Converting to CSV
+    function convertToCSV(data) {
+        const header = ["Data", "Wartosc (dB)", "Antena"];
+        const rows = data.map(d => {
+            return [
+                d.date.toISOString(), 
+                d.close,
+                d.antenna
+            ].join(";");
+        });
+        return [header.join(";"), ...rows].join("\n");
+    }
+
+    const csvData = convertToCSV(filteredData);
+
+    // Creating blob object with csv data
+    const blob = new Blob([csvData], { type: "text/csv" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "data.csv";
+    link.click();
+
+    //Clearing the data
+    URL.revokeObjectURL(url);
+});
+// Button to export the data as JSON
+document.getElementById("export-json").addEventListener("click", () => {
+    if (!filteredData || filteredData.length === 0) {
+        alert("Brak danych do eksportu.");
+        return;
+    }
+
+    const jsonBlob = new Blob([JSON.stringify(filteredData, null, 2)], { type: 'application/json' });
+    const jsonUrl = URL.createObjectURL(jsonBlob);
+    const link = document.createElement("a");
+    link.href = jsonUrl;
+    link.download = 'data.json';
+    link.click();
+});
+
+function addLegend(svg, antennas, colorScale) {
+    const legend = svg.append("g")
+        .attr("transform", `translate(${width - marginRight - 100}, ${marginTop})`)
+        .attr("class", "legend");
+
+    antennas.forEach((antenna, i) => {
+        const legendRow = legend.append("g")
+            .attr("transform", `translate(0, ${i * 20})`);
+
+        // Add rectangle with color
+        legendRow.append("rect")
+            .attr("width", 12)
+            .attr("height", 12)
+            .attr("fill", colorScale(antenna));
+
+        // Add text next to the rectangle
+        legendRow.append("text")
+            .attr("x", 16)
+            .attr("y", 10)
+            .attr("dy", "0.32em")
+            .attr("fill", "currentColor")
+            .style("font-size", "12px")
+            .text(antenna);
+    });
+}
