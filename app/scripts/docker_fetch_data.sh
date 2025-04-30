9178f482e10b6e6c6fa16bd9e504a056662b5ea1
Kacper Capiga <01187312@pw.edu.pl>
2025-04-30 22:49:06 +0200

    fix: minor changes

--- app/scripts/docker_fetch_data.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-docker exec -d proj2-app /app/scripts/fetch_data.sh /Users/kacper/Desktop/Studia/Sem4/PROJ/raw/
--- scripts/fetch_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd $( dirname -- "$( readlink -f -- "$0" )")  # Zmiana katalogu na katalog w którym znajduje się skrypt
-
-for key in `ls ./ssh_keys`; do
-    mkdir -p ../data/raw;
-    rsync -r -e "ssh -i ./ssh_keys/$key" $key:$1 ../data/raw; # Pierwszy argument to lokalizacja, pod którą znajdują się pliki na serwerze
-done
-./update_db.py
